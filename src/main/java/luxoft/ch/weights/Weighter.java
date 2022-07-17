71e8e3c17da2102b8ddd52b5f71821e3d33f6b4d
Serhii Pylypenko <serhii.pylypenko2@dxc.com>
2022-07-17 15:54:15 +0300

    Draft 1

---
+++ src/main/java/luxoft/ch/weights/Weighter.java
@@ -0,0 +1,56 @@
+package luxoft.ch.weights;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Weighter {
+
+	private record Solution(int leftNumber1, int leftNumber2, int rightNumber1, int rightNumber2) {
+		@Override
+		public String toString() {
+			return "[%d,%d - %d,%d]".formatted(leftNumber1, leftNumber2, rightNumber1, rightNumber2);
+		}
+	}
+
+	private final int bigWeight;
+	private final int number1;
+	private final int weight1;
+	private final int number2;
+	private final int weight2;
+
+	public Weighter(int bigWeight, int number1, int weight1, int number2, int weight2) {
+		this.bigWeight = bigWeight;
+		this.number1 = number1;
+		this.weight1 = weight1;
+		this.number2 = number2;
+		this.weight2 = weight2;
+	}
+
+	public List<Solution> solve() {
+		List<Solution> solutions = new ArrayList<>();
+		for (int number1Difference = 0; number1Difference <= number1; number1Difference++) {
+			int number2Total = bigWeight - number1Difference * weight1;
+			if (number2Total % weight2 == 0) {
+				int number2Difference = number2Total / weight2;
+				fillInSolutions(solutions, number1Difference, number2Difference);
+			}
+		}
+		return solutions;
+	}
+
+	private void fillInSolutions(List<Solution> solutions, int number1Difference, int number2Difference) {
+		for (int n1 = 0; n1 * 2 + number1Difference <= number1; n1++) {
+			for (int n2 = 0; n2 * 2 + number2Difference <= number2; n2++) {
+				solutions.add(new Solution(n1, n2, n1 + number1Difference, n2 + number2Difference));
+			}
+		}
+	}
+
+	public static void main(String... args) {
+		var weighter = new Weighter(100, 10, 5, 4, 25);
+		System.out.println(weighter.solve().stream().map(Solution::toString).collect(Collectors.joining("\n")));
+
+	}
+
+}
